aaf45818802f455b1b75c2a1afed55ffaf023c3e
Yashghatage <yashghatage935@gmail.com>
2026-08-20 19:53:40 +0530

    Time: 5 ms (81.83%) | Memory: 47.5 MB (34.13%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -4,7 +4,7 @@ class Solution {
         int ans = 0;
         int ans1 = 0;
 
-        // Count elements of nums1 that appear in nums2
+        
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
                 if (nums1[i] == nums2[j]) {
