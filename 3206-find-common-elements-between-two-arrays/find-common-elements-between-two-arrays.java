b5995d7942270c975e6fa03d1d3d6c004b8208b0
Yashghatage <yashghatage935@gmail.com>
2026-08-20 19:52:57 +0530

    Time: 5 ms (81.83%) | Memory: 47.5 MB (34.13%) - LeetSync

---
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+
+        int ans = 0;
+        int ans1 = 0;
+
+        // Count elements of nums1 that appear in nums2
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+
+        // Count elements of nums2 that appear in nums1
+        for (int i = 0; i < nums2.length; i++) {
+            for (int j = 0; j < nums1.length; j++) {
+                if (nums2[i] == nums1[j]) {
+                    ans1++;
+                    break;
+                }
+            }
+        }
+
+        return new int[]{ans, ans1};
+    }
+}
