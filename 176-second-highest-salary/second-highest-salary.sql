a2d5442856be43ce91ed8c73a6017fc57c65643c
Yashghatage <yashghatage935@gmail.com>
2026-08-25 21:41:46 +0530

    Time: 347 ms (20.16%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT MAX(salary) AS SecondHighestSalary
 FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee);
+WHERE salary < (SELECT MAX(salary) FROM Employee);# Write your MySQL query statement below
